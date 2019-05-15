bf45d11523a691c2cbe3fc8b63d4258e9bdf5e0b
Alec Keeter <aleckeeter94@gmail.com>
2019-05-15 18:56:51 -0400

    Adds static routes, root route, and catch all route to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+
+  # custom routes
+  get "home", to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact_us", to: "pages#contact_us"
+  get "residents", to: "pages#residents"
+  get "events", to: "pages#events"
+  get "announcements", to: "pages#announcements"
+  get "error", to: "pages#error"
+
+  root "pages#home"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # catch all route
+
+  get "*path", to: redirect("/error")
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
