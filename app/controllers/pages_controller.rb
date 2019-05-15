bdcb9b039c9f5649cd16ff5ad846eaa64a7a9ab3
Alec Keeter <aleckeeter94@gmail.com>
2019-05-15 18:58:12 -0400

    Adds static routes to the Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def about
+  end
+
+  def contact_us
+  end
+
+  def residents
+  end
+
+  def events
+  end
+
+  def announcements
+  end
+
+
+end
